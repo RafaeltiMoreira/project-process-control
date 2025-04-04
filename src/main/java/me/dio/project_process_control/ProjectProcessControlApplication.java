b78a8ccb30383ac34e9b0374c2bcbb550527dd59
Rafael Moreira <rafaeltimoreirass@gmail.com>
2025-04-04 01:07:00 -0300

    Initial commit

---
+++ src/main/java/me/dio/project_process_control/ProjectProcessControlApplication.java
@@ -0,0 +1,13 @@
+package me.dio.project_process_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectProcessControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectProcessControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/project_process_control/ProjectProcessControlApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.project_process_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectProcessControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
